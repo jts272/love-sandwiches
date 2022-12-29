d9bd10fd9a5eb2e974301379a3dac2f5c7c903ed
jts272 <jts272@outlook.com>
2022-12-29 23:16:02 +0000

    Revert "Add code to check for 6 values in provided user data"
    This reverts commit 11f75e228632729b2131a782c66a80e4a31ada3c.

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-]
+    ]
 
 CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -19,36 +19,15 @@ SHEET = GSPREAD_CLIENT.open("love_sandwiches")
 
 # print(data)
 
-
 def get_sales_data():
-    """
-    Get sales figures input from the user
-    """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, serparated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+  """
+  Get sales figures input from the user
+  """
+  print("Please enter sales data from the last market.")
+  print("Data should be six numbers, serparated by commas.")
+  print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
-
-    # Return the values provided as a list with split method
-    sales_data = data_str.split(",")
-    # print(sales_data)
-    validate_data(sales_data)
-
-
-def validate_data(values):
-    """
-    Try statement converts all string values to integers. Raises
-    ValueError if strings cannot be converted to integers, or if there
-    aren't exactly 6 values.
-    """
-    try:
-        if len(values) != 6:
-            raise ValueError(
-                f"Exactly 6 values required, you provided {len(values)}"
-            )
-    except ValueError as e:
-        print(f"Invalid data: {e}, please try again.\n")
-
+  data_str = input("Enter your data here: ")
+  print(f"The data provided is {data_str}")
 
 get_sales_data()
